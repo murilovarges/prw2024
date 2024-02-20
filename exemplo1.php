75715c803e2b95a1b41f8f953594218a14d30be2
Murilo <murilo.varges@gmail.com>
2024-02-20 11:48:13 -0300

    Ola mundo php.

---
+++ exemplo1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{background-color: red;font-size: 28px;}
+    </style>
+</head>
+<body>
+    <h1>Murilo</h1>    
+    <?php
+        $nota = 10.5;                
+        echo "<div>Nota final:$nota</div>";
+    ?>
+</body>
+</html>
